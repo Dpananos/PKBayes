cb2bee728a7076033a6e38c68b69d41a0b8b7ae5
Demetri <dpananos@gmail.com>
2019-12-09 12:27:36 -0500

    add non-dimensionalization DGP

--- run_data_gen.py
+++ run_data_gen.py
@@ -21,11 +21,13 @@ if __name__ == "__main__":
     parser.add_argument("--n_continuous_predictors", type=int, default=2)
     parser.add_argument("--n_binary_predictors", type=int, default=2)
     parser.add_argument("--binary_prob", type=float, default=0.33)
-    parser.add_argument("--beta_baseline", nargs=3, default=[np.log(3.3), np.log(0.2), np.log(1)])
+
+    parser.add_argument('--alpha', type=float,  default = 0.5)
     parser.add_argument("--beta_cov", nargs=3, default=[0.075, 0.075, 0.075])
     parser.add_argument("--rfx_cov", nargs=3, default=[0.07, 0.2, 0.2])
     parser.add_argument("--use_normal_rfx", type=int, default=1)
     parser.add_argument("--sigma_obs", type=float, default=0.12)
+    parser.add_argument("--use_delay", type=bool, default=True)
     args = parser.parse_args()
 
     # Create some names for our patients.  Zero pad to keep them to easily order them.  Comes in handy when we compare
@@ -41,17 +43,23 @@ if __name__ == "__main__":
         # If we don't observe at the same time, then observation times are uniformly random between tmin and tmax
         obs_times = [np.sort(np.random.uniform(low=args.tmin, high=args.tmax, size=args.n_obs)) for j in range(args.n_subjects)]
         # Store the times in a flat list
-        times = functools.reduce(lambda x, y: x + y, obs_times)
+        times = list(np.concatenate(obs_times))
 
     # See utils/pthon_tools.py
     X = generate_covariates(args.n_subjects, args.n_continuous_predictors,
     args.n_binary_predictors, args.binary_prob)
     betas = generate_regression_coefficients(X, args.beta_cov)
-    pk = generate_pk_params(X, betas, args.beta_baseline, args.use_normal_rfx, args.rfx_cov)
+    # Baseline variables are Cl, ke, ka
+    # Note that ka>ke.  The non-dimensionalization y = D/Cl*C and tau = ka*t yields a nd constant
+    # between 0 and 1 (alpha = ke/ka), or alternatively alpha>1 if alpha = ka/ke.  
+    # On the log space, multuplication is addition
+    # so ka=(1/alpha)*ke  ==> exp(log(ka)) = exp(log(1/alpha*ke)) = exp(log(1/alpha) + log(ke))
+    beta_baseline = [np.log(3.3), np.log(1/args.alpha) + np.log(0.5), np.log(0.5)] 
+    pk = generate_pk_params(X, betas, beta_baseline, args.use_normal_rfx, args.rfx_cov)
 
     # Observational model is lognormal
     # PK function is y' = (D/Cl)*ke*ka*exp(-ka*t) - ke*y
-    df = make_obs(args.D, subjects, times, X, pk, args.sigma_obs)
+    df = make_obs(args.D, subjects, times, X, pk, args.sigma_obs, use_delay=args.use_delay)
     df.to_csv("data/test.csv")
 
     with open('data/reg_coefs.txt', 'wb') as f:
--- utils/python_tools.py
+++ utils/python_tools.py
@@ -48,15 +48,23 @@ def pk_func(t,D,Cl, ka, ke):
     return (D/Cl)*(ke*ka/(ke-ka))*(np.exp(-ka*t) - np.exp(-ke*t))
 
 
-def make_obs(D, subjects, times, X, pk, sigma_obs):
+def make_obs(D, subjects, times, X, pk, sigma_obs, use_delay = False):
 
     unique_subjects = np.sort(np.unique(subjects))
     pkdf = pd.DataFrame(pk, columns=['Cl','ka','ke'], index=unique_subjects )
     tdf = pd.DataFrame(times, columns=['t'], index=subjects)
     Xdf = pd.DataFrame(X, columns=[f'x_{j}' for j in range(X.shape[1])], index=unique_subjects )
 
+    # Delay in drug absorption
     df = tdf.join(pkdf, how='left').join(Xdf, how='left')
-    df['y'] = pk_func(df.t, D, df.Cl, df.ka, df.ke )
+    if use_delay:   
+        delays = pm.Beta.dist(alpha=10, beta=30).random(size=unique_subjects.size)
+        dfdelay = pd.DataFrame({'delay':delays}, index = unique_subjects)
+        df = df.join(dfdelay, how='left')
+        df['y'] = pk_func(df.t-df.delay, D, df.Cl, df.ka, df.ke )
+    else:
+        df['y'] = pk_func(df.t, D, df.Cl, df.ka, df.ke )
+
     z = norm(loc=0, scale=sigma_obs)
     df['y_obs'] = np.exp( np.log(df.y) + z.rvs(size=df.shape[0], random_state= 0) )
 
